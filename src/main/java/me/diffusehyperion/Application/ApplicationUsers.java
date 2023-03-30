60fa91b662e74b0c0e89a2f7c3474d3f1acef11d
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2023-03-30 07:27:48 +0000

    added applicationusers class

---
+++ src/main/java/me/diffusehyperion/Application/ApplicationUsers.java
@@ -0,0 +1,84 @@
+package me.diffusehyperion.Application;
+
+import me.diffusehyperion.PterodactylAPI;
+
+public class ApplicationUsers {
+
+    private Application application;
+    private int id;
+    private String externalId;
+    private String uuid;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String language;
+    private boolean rootAdmin;
+    private boolean twoFactorAuthentication;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public UserAttributes(Application application, JSONObject object) {
+        this.application = application;
+        this.id = (int) object.get("id");
+        this.externalId = (String) object.get("external_id");
+        this.uuid = (String) object.get("uuid");
+        this.username = (String) object.get("username");
+        this.email = (String) object.get("email");
+        this.firstName = (String) object.get("first_name");
+        this.lastName =(String)  object.get("last_name");
+        this.language = (String) object.get("language");
+        this.rootAdmin = (boolean) object.get("root_admin").getAsBoolean();
+        this.twoFactorAuthentication = (boolean) object.get("2fa").getAsBoolean();
+        this.createdAt = LocalDateTime.parse((String) object.get("created_at"));
+        this.updatedAt = LocalDateTime.parse((String) object.get("updated_at"));
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getExternalId() {
+        return externalId;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public boolean isRootAdmin() {
+        return rootAdmin;
+    }
+
+    public boolean isTwoFactorAuthentication() {
+        return twoFactorAuthentication;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+}
